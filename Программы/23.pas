ee50171de78b82c4caba4058c9e414c6e7f0947a
fedorovalex <INOY310@yandex.ru>
2020-05-06 20:40:28 +0300

    Реализована задача 23

--- Программы/23.pas
+++ Программы/23.pas
@@ -1,15 +1,20 @@
 ﻿// Опредиление кол-ва одинаковых цифр в трёхзначном числе
 
 program n_23;
-var coincidence, number, a,d, c: integer;
+var
+  coincidence, number, a, b, c: integer;
 
 begin 
-  write ('Введите число ');
-  read (number);
-  
-  
-  
-  
-  
+  write('Введите число ');
+  read(number);
+  a := number mod 10;
+  number := number div 10;
+  b := number mod 10;
+  c := number div 10;
+  if (a = b) or (b = c) or (c = a) then begin
+    write('Есть');  
+  end else begin
+    write('Нету');
+  end;
   
 end.
