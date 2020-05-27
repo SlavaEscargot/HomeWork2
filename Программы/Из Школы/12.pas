16930f6cd11f3f1feea604283ea7a46e1f652b0e
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-05-27 18:49:17 +0300

    Create 12.pas

---
+++ Программы/Из Школы/12.pas
@@ -0,0 +1,17 @@
+﻿//Программа, которая вычислдяет сумму кодов букв в слове
+program n12;
+ 
+var 
+  term: string; 
+  numberOfLetters, i: integer;
+  
+begin 
+  term := readString('Введите слово');  
+  numberOfLetters := length(term);
+  while i <= numberOfLetters do begin
+        
+        i := i + 1;
+  end;
+
+  
+end.  
