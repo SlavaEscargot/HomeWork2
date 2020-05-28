97d8b8a6db425eaa62bbe5e5ac9c3eeb26f777b5
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-05-28 18:42:01 +0300

    Реализовал номер 30

---
+++ Программы/30 амёбы.pas
@@ -0,0 +1,14 @@
+﻿//программа по вычесления времени через которое будет х амёб.
+program n30;
+var
+  amoeba,userAmoebas, time: integer;
+  
+begin 
+  userAmoebas := readInteger('Введите количества амёб');
+  amoeba := 1;
+  while amoeba < userAmoebas do begin
+    amoeba += 2;
+    time += 3;
+  end;
+  writeln('через ', time, ' часов будет ', userAmoebas , ' амёб'); 
+end.  
