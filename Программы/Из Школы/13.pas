1cb81c35d65058ebb92c905ea80f9c0e6d717bec
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-06-02 18:17:32 +0300

    Не работает 13

---
+++ Программы/Из Школы/13.pas
@@ -0,0 +1,12 @@
+﻿//
+program n13;
+var
+  code: array [1..1] of integer =(66);
+  i, amountOfCode: integer;
+  setOfLetters: char;
+begin
+//  for i := 1 to amountOfCode do begin
+    setOfLetters := chr([i]);
+    write(setOfLetters); 
+//  end;
+end. 
