e51d7d2d51ecbcdd1ae0a0e50c70fe8ddae9cc46
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-07-09 18:40:53 +0300

    Update 13.pas
    Преобразовал код в букву

--- Программы/Из Школы/13.pas
+++ Программы/Из Школы/13.pas
@@ -1,12 +1,12 @@
-﻿//
+﻿//Программа из кодов букв делает слова.
 program n13;
 var
-  code: array [1..1] of integer =(66);
+  code: array [1..3] of integer =(66, 55, 76);
   i, amountOfCode: integer;
   setOfLetters: char;
 begin
-//  for i := 1 to amountOfCode do begin
-    setOfLetters := chr([i]);
+  for i := 1 to 3 do begin
+    setOfLetters := chr(code[i]);
     write(setOfLetters); 
-//  end;
+  end;
 end. 
