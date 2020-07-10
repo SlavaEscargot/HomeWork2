63d15982031e410d5ee1a9116f523a69a3c54a8a
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-07-10 10:37:26 +0300

    Реализавал номер13

--- Программы/Из Школы/13.pas
+++ Программы/Из Школы/13.pas
@@ -1,11 +1,12 @@
 ﻿//Программа из кодов букв делает слова.
 program n13;
+const amountOfCode = 3
 var
-  code: array [1..3] of integer =(66, 55, 76);
-  i, amountOfCode: integer;
+  code: array [1..amountOfCode] of integer =(66, 55, 76);
+  i : integer;
   setOfLetters: char;
 begin
-  for i := 1 to 3 do begin
+  for i := 1 to amountOfCode do begin
     setOfLetters := chr(code[i]);
     write(setOfLetters); 
   end;
