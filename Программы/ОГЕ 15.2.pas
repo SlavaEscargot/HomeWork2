03d6b5e988a66ffb7df50eb58c1c338b32adb613
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-08-18 15:54:21 +0300

    Create ОГЕ 15.2.pas

---
+++ Программы/ОГЕ 15.2.pas
@@ -0,0 +1,17 @@
+﻿// Определение количества чётных трёхзначных чисел.
+
+program n_15_2;
+var
+  theNumberOfUser, answer: integer;
+begin
+  repeat
+    theNumberOfUser:= readInteger('Введите число');
+    if (theNumberOfUser > 99) and (theNumberOfUser < 1000) then begin
+      if (theNumberOfUser mod 10 = 0) or (theNumberOfUser mod 10 = 2) or (theNumberOfUser mod 10 = 4) or (theNumberOfUser mod 10 = 6) or (theNumberOfUser mod 10 = 8)then begin
+        answer += 1;
+         end;
+    end;
+  until theNumberOfUser = 0;
+  write(answer);
+  
+end.
