a6257316fa5b9c21c65edf73353d181797a94d1c
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2020-10-17 18:36:41 +0300

    Исправил  информат ику

--- Огэ/Информатика/2/Программа.pas
+++ Огэ/Информатика/2/Программа.pas
@@ -3,8 +3,8 @@ var
   a, b:integer;
 begin
   repeat
-    read (a);
-    if (a<1000) and (a>b) then begin
+    read(a);
+    if (a < 1000) and (a > b) then begin
       b := a; 
     end;   
   until a = 0 ;
