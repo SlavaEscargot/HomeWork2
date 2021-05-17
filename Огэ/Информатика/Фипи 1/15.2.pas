31a3f797a02659073e7719210bde170b10094aa9
SlavaEscargot <64158249+SlavaEscargot@users.noreply.github.com>
2021-05-17 20:09:10 +0300

    Программа

---
+++ Огэ/Информатика/Фипи 1/15.2.pas
@@ -0,0 +1,10 @@
+﻿Program n15;
+Var nemberUser, i, quanity,: integer;
+Begin
+  writeln('Введите Кол-во чисел');
+  readln(nemberUser);
+  while i <= nemberUser do begin
+        i := i + 1;
+  end;
+
+end. 
